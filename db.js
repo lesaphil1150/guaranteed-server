0d40afb3ea4fc03f44ea3b78795f07ec6cebd0cd
lesaphil1150 <lesaphil1150@gmail.com>
2021-11-04 19:46:57 -0400

    second push

--- db.js
+++ db.js
@@ -1,17 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("guaranteed", "postgres", "password", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: !process.env.DATABASE_URL.includes("localhost")
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // fixing unhandled rejection
+        },
+      }
+    : {},
 });
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully");
-//   })
-//   .catch((err) => {
-//     console.error("unable to connect to database:", err);
-//   });
 
 module.exports = sequelize;
